766fac736a9105062219c7167fb1d7c3d445fdd8
walker <1156199735@qq.com>
2025-11-24 20:14:51 +0800

    second commit

--- redis-demo1/src/main/java/com/xzit/RedisDemo1Application.java
+++ redis-demo1/src/main/java/com/xzit/RedisDemo1Application.java
@@ -1,13 +1,22 @@
 package com.xzit;
 
+import io.lettuce.core.ReadFrom;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RedisDemo1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(RedisDemo1Application.class, args);
+    }
+
+    //配置哨兵模式下，优先从从节点读数据，再从主节点读数据
+    @Bean
+    public LettuceClientConfigurationBuilderCustomizer clientConfigurationBuilderCustomizer(){
+        return clientConfigurationBuilder -> clientConfigurationBuilder.readFrom(ReadFrom.REPLICA_PREFERRED);
     }
 
 }
